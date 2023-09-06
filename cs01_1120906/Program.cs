e48d834b1d793321bc088580cbdcd3d332075488
Surgexuan1208 <jeffs941208@gmail.com>
2023-09-06 23:31:35 +0800

    m*m乘法表

--- cs01_1120906/Program.cs
+++ cs01_1120906/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("請輸入一個整數n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++) { 
+                Console.WriteLine($"{i}*{j}={i * j}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
